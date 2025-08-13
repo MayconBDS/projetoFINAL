4ba2f1615fccb03d7e57b66daa2b12eea5995f5a
MayconBDS <mayconbarradas7@gmail.com>
2025-08-13 01:31:59 -0300

    Add files via upload

---
+++ animan/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib import admin
+
+urlpatterns = [
+    path('', include('animes.urls')),         
+    path('reviews/', include('reviews.urls')), 
+    path('', include('usuarios.urls')),        
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ animes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Anime 
+
+@login_required
+def home(request):
+    animes = Anime.objects.all()[:10]
+    return render(request, 'animes/home.html', {'animes': animes})
+
+def anime_detalhes(request, anime_id):
+    anime = get_object_or_404(Anime, id=anime_id)
+    return render(request, 'animes/detalhes.html', {'anime': anime})
+
